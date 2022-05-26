6815f6a235ec42bf2899ac0511d535afedd368c8
saba liluashvili <sabaliluashvili12@gmail.com>
2022-05-26 16:54:15 +0400

    fixed mail problems

--- SuperMarketTHEREALONE/Cart.cs
+++ SuperMarketTHEREALONE/Cart.cs
@@ -24,20 +24,29 @@ namespace SuperMarketTHEREALONE
 
         private void btnBuy_Click(object sender, EventArgs e)
         {
-            using (MailMessage mail = new MailMessage())
+            try
             {
-                mail.From = new MailAddress("sabaliluashvili@mziuri.ge");
-                mail.To.Add(Emailtxt.Text);
-                mail.Subject = Nametxt.Text+Surnametxt.Text +"Receipt for the purchase:";
-                mail.Body = "Are you trying to buy the follow items: \n" + cart +"\n"+Convert.ToString(full)+"$$\n";
-                mail.IsBodyHtml = true;
-                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+
+
+                using (MailMessage mail = new MailMessage())
                 {
-                    smtp.Credentials = new System.Net.NetworkCredential("sabaliluashvili@mziuri.ge", "MZIURI2015");
-                    smtp.EnableSsl = true;
-                    smtp.Send(mail);
-                    label1.Text = "SENT";
+                    mail.From = new MailAddress("diaxmasgisment@gmail.com");
+                    mail.To.Add(Emailtxt.Text);
+                    mail.Subject = Nametxt.Text + Surnametxt.Text + "Receipt for the purchase:";
+                    mail.Body = "Are you trying to buy the follow items: \n" + cart + "\n" + Convert.ToString(full) + "$$\n";
+                    mail.IsBodyHtml = true;
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new System.Net.NetworkCredential("diaxmasgisment@gmail.com", "michedavs");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                        label1.Text = "SENT";
+                    }
                 }
+            }
+            catch 
+            {
+                MessageBox.Show("sheiyvanet swore meil");
             }
         }
 
